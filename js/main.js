b4887ffb763cd911aa428c2573395c153eeb06c2
keemsunhee <shon0111110@gmail.com>
2025-03-06 17:12:53 +0900

    섹션3. 스크롤을 이용한 인터랙션 구현(스크롤 세팅)

---
+++ js/main.js
@@ -0,0 +1,91 @@
+(() => {
+  let yOffset = 0;
+  let prevScrollHeight = 0;
+  let currentScene = 0;
+  const sceneInfo = [
+    {
+      //0
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-0"),
+      },
+    },
+    {
+      //1
+      type: "normal",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-1"),
+      },
+    },
+    {
+      //2
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-2"),
+      },
+    },
+    {
+      //3
+      type: "sticky",
+      heightNum: 5,
+      scrollHeight: 0,
+      objs: {
+        container: document.querySelector("#scroll-section-3"),
+      },
+    },
+  ];
+
+  function setLayout() {
+    // 각 스크롤 섹션의 높이 세팅
+    for (let i = 0; i < sceneInfo.length; i++) {
+      sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
+      sceneInfo[
+        i
+      ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
+    }
+
+    let totalScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight;
+      if (totalScrollHeight >= pageYOffset) {
+        currentScene = i;
+        break;
+      }
+    }
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
+  }
+
+  function scrollLoop() {
+    prevScrollHeight = 0;
+    for (let i = 0; i < sceneInfo.length; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+
+      currentScene--;
+    }
+
+    document.body.setAttribute("id", `show-scene-${currentScene}`);
+  }
+
+  window.addEventListener("scroll", () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
+
+  //   window.addEventListener("DonContentLoaded", setLayout);
+  window.addEventListener("load", setLayout);
+  window.addEventListener("resize", setLayout);
+})();
